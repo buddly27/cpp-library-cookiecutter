d3baac4f3cfb2370c2a6b77a87f0d8486736ea21
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2022-08-23 18:20:13 -0700

    Improve API reference display

--- {{cookiecutter.repository}}/src/{{cookiecutter.library_header_subfolder}}/fibonacci.h
+++ {{cookiecutter.repository}}/src/{{cookiecutter.library_header_subfolder}}/fibonacci.h
@@ -1,8 +1,13 @@
 #ifndef {{ cookiecutter.project.replace(" ", "_").upper() }}_EXAMPLE_H
 #define {{ cookiecutter.project.replace(" ", "_").upper() }}_EXAMPLE_H
+/// @file fibonacci.h
+/// @brief This is an example.
 
 namespace {{ cookiecutter.cpp_namespace }} {
 
+/// Compute number at the *n* position on the Fibonacci sequence.
+/// \param n Position of the number to compute.
+/// \returns Computed number.
 int fibonacci(unsigned int n);
 
 } // namespace {{ cookiecutter.cpp_namespace }}
